51a9261c1e8b7f7759f15a002c650faabc30533c
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-03-11 14:52:47 +0330

    Update tests

--- AVX-Hole/tests/test_avx2_double.hxx
+++ AVX-Hole/tests/test_avx2_double.hxx
@@ -12,7 +12,6 @@ int test_avx2_double_set_zero_and_store();
 int test_avx2_double_set_scalar();
 int test_avx2_double_broadcast();
 int test_avx2_double_load();
-int test_avx2_double_store();
 int test_avx2_double_add();
 int test_avx2_double_sub();
 int test_avx2_double_mul();
