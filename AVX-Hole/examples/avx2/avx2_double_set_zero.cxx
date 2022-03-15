c8354acedfc8849523ca6305f45573dfdfdb5a53
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-03-15 12:51:39 +0330

    Add examples

---
+++ AVX-Hole/examples/avx2/avx2_double_set_zero.cxx
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <iostream>
+#include <vector>
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+
+int main() {
+	std::cout << "\nSIMD AVX2 Double Set Zero Example." << std::endl;
+
+	// Container to store solution
+	std::vector<double> a(4);
+
+	// Define SIMD object of zeros
+	auto va = avxhole::simd::avx2_set_zero<double>();
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(a.data(), va);
+
+	// Display result
+	avxhole::util::print_vector("\na", a.size(), a.data(), 1, 1);
+
+	// a = [
+	//  0 0 0 0
+	// ]
+}
