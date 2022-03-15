7a3cfe4c1ce4ae486a62e10d3919918c6cfd5af6
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-03-15 12:49:00 +0330

    Add examples

---
+++ AVX-Hole/examples/avx2/avx2_double_reduce.cxx
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <iostream>
+#include <vector>
+#include <avxhole/simd.hxx>
+
+
+int main() {
+	std::cout << "\nSIMD AVX2 Double Reduce Example." << std::endl;
+
+	// Input data
+	std::vector<double> a{ 1.1, 4.9, 2.3, 3.5 };
+
+	// Define SIMD object using input data
+	auto va = avxhole::simd::avx2_load(a.data());
+
+	// Compute sum of all elements in SIMD object
+	double r = avxhole::simd::avx2_reduce(va);
+
+	// Display result
+	std::cout << "\nr = " << r << std::endl;  // r = 11.8
+}
