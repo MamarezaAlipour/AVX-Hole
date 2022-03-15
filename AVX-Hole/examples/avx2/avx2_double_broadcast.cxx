e207dafc280f79378d8df1c38176eac3010faa1f
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-03-15 12:31:14 +0330

    add examples

---
+++ AVX-Hole/examples/avx2/avx2_double_broadcast.cxx
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <iostream>
+#include <vector>
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+
+int main() {
+	std::cout << "\nSIMD AVX2 Double Broadcast Example." << std::endl;
+
+	// Container to store solution
+	std::vector<double> a(4);
+
+	// Scalar value
+	double s = 5.5;
+
+	// Broadcast scalar value to each element of SIMD object
+	auto va = avxhole::simd::avx2_broadcast(&s);
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(a.data(), va);
+
+	// Display result
+	avxhole::util::print_vector("\na", a.size(), a.data(), 2, 3);
+
+	// a = [
+	//  5.5 5.5 5.5 5.5
+	// ]
+}
