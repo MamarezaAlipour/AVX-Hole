e90fd00f4a765dc8c9e3cf8a604402cea132bb43
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-14 21:45:20 +0430

    Add examples

---
+++ AVX-Hole/examples/avx2/avx2_float_mul.cxx
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+
+
+int main() {
+	std::cout << "\nSIMD AVX2 Float Multiplication Example." << std::endl;
+
+	// Input data
+	std::vector<float> a{ 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5 };
+	std::vector<float> b{ 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2 };
+
+	// Container to store solution
+	std::vector<float> c(8);
+
+	// Define SIMD objects using input data
+	auto va = avxhole::simd::avx2_load(a.data());
+	auto vb = avxhole::simd::avx2_load(b.data());
+
+	// Compute SIMD operation c = a * b
+	auto vc = avxhole::simd::avx2_mul(va, vb);
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(c.data(), vc);
+
+	// Display result
+	avxhole::util::print_vector("\nc", c.size(), c.data(), 3, 4);
+
+	// c = [
+	//  12.1 12.1 12.1 12.1 12.1 12.1 12.1 12.1
+	// ]
+}
