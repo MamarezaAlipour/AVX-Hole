4c020b2fedaf2b530474285004f43a8a64434332
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-14 21:44:18 +0430

    Add examples

---
+++ AVX-Hole/examples/avx2/avx2_float_load.cxx
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <iostream>
+#include <vector>
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+
+
+int main() {
+	std::cout << "\nSIMD AVX2 Float Load Example." << std::endl;
+
+	// Input data
+	std::vector<float> a{ 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5 };
+
+	// Container to store solution
+	std::vector<float> b(8);
+
+	// Define SIMD object using input data
+	auto va = avxhole::simd::avx2_load(a.data());
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(b.data(), va);
+
+	// Display result
+	avxhole::util::print_vector("\nb", b.size(), b.data(), 2, 3);
+
+	// b = [
+	//  5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5
+	// ]
+}
