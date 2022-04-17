5024ee86e55ad6b017084502b55fe963726e9d3d
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-17 15:00:27 +0430

    Add tests

---
+++ AVX-Hole/examples/avx2/avx2_float_sub.cxx
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+#include <vector>
+
+int main() {
+	std::cout << "\nSIMD AVX2 Float Subtraction Example." << std::endl;
+
+	// Input data
+	std::vector<float> a {5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
+	std::vector<float> b {2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2};
+
+	// Container to store solution
+	std::vector<float> c(8);
+
+	// Define SIMD objects using input data
+	auto va = avxhole::simd::avx2_load(a.data());
+	auto vb = avxhole::simd::avx2_load(b.data());
+
+	// Compute SIMD operation c = a - b
+	auto cv = avxhole::simd::avx2_sub(va, vb);
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(c.data(), cv);
+
+	// Display result
+	avxhole::util::print_vector("\nc", c.size(), c.data(), 2, 3);
+
+	// c = [
+	//  3.3 3.3 3.3 3.3 3.3 3.3 3.3 3.3
+	// ]
+}
