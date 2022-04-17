9616d72d81703b965f1e8d144dfd3d73836e882e
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-17 21:51:34 +0430

    Add tests

---
+++ AVX-Hole/examples/avx512/avx512_double_div.cxx
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout << "\nSIMD AVX512 Double Division Example." << std::endl;
+
+	// Input data
+	std::vector<double> a {5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
+	std::vector<double> b {2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2};
+
+	// Container to store solution
+	std::vector<double> c(8);
+
+	// Define SIMD objects using input data
+	auto va = avxhole::simd::avx512_load(a.data());
+	auto vb = avxhole::simd::avx512_load(b.data());
+
+	// Compute SIMD operation c = a / b
+	auto vc = avxhole::simd::avx512_div(va, vb);
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx512_store(c.data(), vc);
+
+	// Display result
+	avxhole::util::print_vector("\nc", c.size(), c.data(), 2, 3);
+
+	// c = [
+	//  2.5 2.5 2.5 2.5 2.5 2.5 2.5 2.5
+	// ]
+}
