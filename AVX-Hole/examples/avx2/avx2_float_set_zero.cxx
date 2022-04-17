b97899a01bec8647b34a38d3303e9f798b6bec31
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-17 14:58:17 +0430

    Add tests

---
+++ AVX-Hole/examples/avx2/avx2_float_set_zero.cxx
@@ -0,0 +1,24 @@
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout << "\nSIMD AVX2 Float Set Zero Example." << std::endl;
+
+	// Container to store solution
+	std::vector<float> a(8);
+
+	// Define SIMD object of zeros
+	auto va = avxhole::simd::avx2_set_zero<float>();
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx2_store(a.data(), va);
+
+	// Display result
+	avxhole::util::print_vector("\na", a.size(), a.data(), 1, 1);
+
+	// a = [
+	//  0 0 0 0 0 0 0 0
+	// ]
+}
