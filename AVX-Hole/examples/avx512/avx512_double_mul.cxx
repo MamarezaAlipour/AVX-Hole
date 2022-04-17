f1e80e92a185523e5536f2aaee3a4629eb46baba
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-17 22:03:43 +0430

    Add tests

--- AVX-Hole/examples/avx512/avx512_double_mul.cxx
+++ AVX-Hole/examples/avx512/avx512_double_mul.cxx
@@ -1,37 +1,35 @@
 // Copyright (c) 2022 Parisa Khaleghi
 // All rights reserved
 
-#include <iostream>
-#include <vector>
 #include <avxhole/simd.hxx>
 #include <avxhole/util.hxx>
+#include <iostream>
+#include <vector>
 
+int main() {
+	std::cout << "\nSIMD AVX512 Double Multiplication Example." << std::endl;
 
-int main()
-{
-  std::cout << "\nSIMD AVX512 Double Multiplication Example." << std::endl;
+	// Input data
+	std::vector<double> a {5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
+	std::vector<double> b {2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2};
 
-  // Input data
-  std::vector<double> a{ 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5 };
-  std::vector<double> b{ 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2, 2.2 };
+	// Container to store solution
+	std::vector<double> c(8);
 
-  // Container to store solution
-  std::vector<double> c(8);
+	// Define SIMD objects using input data
+	auto va = avxhole::simd::avx512_load(a.data());
+	auto vb = avxhole::simd::avx512_load(b.data());
 
-  // Define SIMD objects using input data
-  auto va = avxhole::simd::avx512_load( a.data() );
-  auto vb = avxhole::simd::avx512_load( b.data() );
+	// Compute SIMD operation c = a * b
+	auto vc = avxhole::simd::avx512_mul(va, vb);
 
-  // Compute SIMD operation c = a * b
-  auto vc = avxhole::simd::avx512_mul(va, vb);
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx512_store(c.data(), vc);
 
-  // Transfer data from SIMD object to container
-  avxhole::simd::avx512_store(c.data(), vc);
+	// Display result
+	avxhole::util::print_vector("\nc", c.size(), c.data(), 3, 4);
 
-  // Display result
-  avxhole::util::print_vector("\nc", c.size(), c.data(), 3, 4);
-
-  // c = [
-  //  12.1 12.1 12.1 12.1 12.1 12.1 12.1 12.1
-  // ]
+	// c = [
+	//  12.1 12.1 12.1 12.1 12.1 12.1 12.1 12.1
+	// ]
 }
