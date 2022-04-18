4d7a45c81b0cdbf0523da8b8f3aaa6280df148c0
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-18 13:24:20 +0430

    Add tests

---
+++ AVX-Hole/examples/avx512/avx512_float_store.cxx
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#include <avxhole/simd.hxx>
+#include <avxhole/util.hxx>
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout << "\nSIMD AVX512 Float Store Example." << std::endl;
+
+	// Input data
+	std::vector<float> a {5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5,
+						  5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
+
+	// Container to store solution
+	std::vector<float> b(16);
+
+	// Define SIMD object using input data
+	auto va = avxhole::simd::avx512_load(a.data());
+
+	// Transfer data from SIMD object to container
+	avxhole::simd::avx512_store(b.data(), va);
+
+	// Display result
+	avxhole::util::print_vector("\nb", b.size(), b.data(), 2, 3);
+
+	// b = [
+	//  5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5 5.5
+	// ]
+}
