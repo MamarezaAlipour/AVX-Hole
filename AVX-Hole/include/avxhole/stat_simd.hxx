b2f84c1bfc9a0b49c55e4884c37b06270727339f
Parisa Khaleghi <parisakhaleghi97@gmail.com>
2022-04-19 18:55:58 +0430

    include base: Added

---
+++ AVX-Hole/include/avxhole/stat_simd.hxx
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Parisa Khaleghi
+// All rights reserved
+
+#ifndef AVXHOLE_STAT_SIMD_HXX
+#define AVXHOLE_STAT_SIMD_HXX
+
+#include <avxhole/stat/stat_impl_avx2.hxx>
+#include <avxhole/stat/stat_impl_avx512.hxx>
+#include <avxhole/stat/stat_impl_serial.hxx>
+#include <avxhole/stat/stat_prng.hxx>
+
+#endif
